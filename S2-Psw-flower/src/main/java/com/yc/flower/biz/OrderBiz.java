88ed7e54edd4c452b442fbf8ed0c277aad52244b
Lenovo <Lenovo@DESKTOP-J230CP2>
2020-12-23 13:55:31 +0800

    购物车

---
+++ S2-Psw-flower/src/main/java/com/yc/flower/biz/OrderBiz.java
@@ -0,0 +1,48 @@
+package com.yc.flower.biz;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yc.flower.bean.Order;
+import com.yc.flower.dao.CartDao;
+import com.yc.flower.dao.OrderDao;
+import com.yc.flower.util.Utils;
+
+
+
+@Service
+public class OrderBiz {
+
+	@Resource
+	private CartDao cdao;
+	
+	@Resource
+	private OrderDao odao;
+	
+	@Transactional
+	public void pay(Order order) throws BizException{
+
+		// 验证输入值
+		Utils.checkNull(order.getAddr(), "请填写收货地址");
+		Utils.checkNull(order.getPhone(), "请填写收货人电话");
+		Utils.checkNull(order.getName(), "请填写收货人姓名");
+		
+		// 计算总金额
+		Double total = cdao.selectTotalByUid(order.getUid());
+		order.setTotal(total);
+		order.setState(0);
+		
+		// 写入数据库
+		// 订单主表  orders
+		int oid = odao.insertOrder(order);
+		// 订单明细 orderitem
+		order.setOid(oid);
+		odao.insertItems(order);
+		// 清空购物车 cart ==> uid
+		cdao.deleteByUid(order.getUid());
+
+	}
+	
+}
