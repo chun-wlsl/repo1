ae8e209dbd209cbe6bb42e341ee31853a0d68306
Lenovo <Lenovo@DESKTOP-J230CP2>
2020-12-26 15:44:35 +0800

    验证码

---
+++ S2-Psw-flower/src/main/java/com/yc/flower/biz/BankBiz.java
@@ -0,0 +1,17 @@
+package com.yc.flower.biz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankBiz {
+
+	/**
+	 * 根据用户名发送邮件，并且将生成的验证码返回
+	 * @param name
+	 * @return*/
+	//public String sendVcode(String name) {
+		//根据用户名获取用户信息
+		
+		//
+	//}
+}
