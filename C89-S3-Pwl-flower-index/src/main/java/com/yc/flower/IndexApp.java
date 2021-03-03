e914126f40e055803395e519aaf4907d8b0a1ad7
1079368619 <1079368619@qq.com>
2021-03-03 16:08:52 +0800

    负载均衡

--- C89-S3-Pwl-flower-index/src/main/java/com/yc/flower/IndexApp.java
+++ C89-S3-Pwl-flower-index/src/main/java/com/yc/flower/IndexApp.java
@@ -2,8 +2,11 @@ package com.yc.flower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,5 +16,12 @@ public class IndexApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndexApp.class, args);
+	}
+	
+	// 负载均衡注解
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
--- C89-S3-Pwl-flower-user/src/main/java/com/yc/flower/UserApp.java
+++ C89-S3-Pwl-flower-user/src/main/java/com/yc/flower/UserApp.java
@@ -3,8 +3,13 @@ package com.yc.flower;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("com.yc.flower.dao")
@@ -12,5 +17,12 @@ public class UserApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApp.class, args);
+	}
+
+	// 负载均衡注解
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
